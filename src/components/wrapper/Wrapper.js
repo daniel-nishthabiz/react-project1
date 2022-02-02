aa7667a87212ae97db380f932cfdbe020b13a391
daniel-nishthabiz <98528923+daniel-nishthabiz@users.noreply.github.com>
2022-02-02 14:19:53 +0000

    Added error and load functionality to wrapper component

--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import styles from "./Wrapper.module.css";
 
-const Wrapper = ({ children, className }) => {
+const Wrapper = ({ children, className, error = false, loaded = true }) => {
     // .trim() to make the class names look nicer in the html
     // nothing else :)
-    return <div className={`${styles.wrapper} ${className}`.trim()}>{children}</div>;
+    useEffect(() => {
+        error && console.log("Changing to error page, error has occurred", error);
+    }, [error]);
+    return (
+        <div className={`${styles.wrapper} ${className ?? ""}`.trim()}>
+            {/* If errored show error occured */}
+            {/* ir not errored and loaded show children */}
+            {/* else show loading page */}
+            {!error ? loaded ? children : <p>loading</p> : <p>An error has occured.</p>}
+        </div>
+    );
 };
 
 export default Wrapper;
